5842acbe3ce461c1379e842cb2362f742523cc42
Caterina <danizio@outlook.it>
2020-10-20 17:57:13 +0200

    Correzione sintassi

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 // Chiedere all'utente quanti km vuole percorrere in treno
 
-var km = parseInt(prompt("Inserisci il numero di km che desideri percorrere in treno"))
+var km = parseInt(prompt("Inserisci il numero di km che desideri percorrere in treno"));
 
-console.log(km)
+console.log(km);
 
 // Chiedere all'utente quanti anni ha
 
-var eta = parseInt(prompt("Quanti anni hai?"))
+var eta = parseInt(prompt("Quanti anni hai?"));
 
-console.log(eta)
+console.log(eta);
 
 // Calcolo del prezzo totale del biglietto per km ('0,21 euro per km')
 
-var prezzoIntero = (km * 0.21)
-console.log(prezzoIntero)
+var prezzoIntero = (km * 0.21);
+console.log(prezzoIntero);
 
-var prezzoTreno
+var prezzoTreno;
 
 // Calcolo dello sconto da applicare
 
    // se il passeggero è minorenne (20% di sconto)
 
    var prezzoMinore = (prezzoIntero - (prezzoIntero * 0.20));
-   console.log(prezzoMinore)
+   console.log(prezzoMinore);
 
    // se il passeggero è over 65 anni (40% di sconto)
 
@@ -30,15 +30,15 @@ var prezzoTreno
    console.log(prezzoOver);
 
 // Output: comunicazione dell'importo totale
-  if (eta >= 65) { (prezzoTreno = prezzoOver)
+  if (eta >= 65) { (prezzoTreno = prezzoOver);
     console.log(prezzoOver);
   }
 
-  if else (eta <= 17) { (prezzoTreno = prezzoMinore)
+  else if (eta <= 17) { (prezzoTreno = prezzoMinore);
     console.log(prezzoMinore);
   }
 
-  else (eta >= 18) { (prezzoTreno = prezzoIntero)
+  else (eta >= 18) { (prezzoTreno = prezzoIntero);
     console.log(prezzoIntero);
   }
 
